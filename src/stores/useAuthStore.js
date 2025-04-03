3a87a5238143a5072fa63304c28e65e20c61d82d
Anushka Prajapati <anushkaprajapati827@gmail.com>
2025-04-03 20:04:49 +0530

    changed token name

--- src/stores/useAuthStore.js
+++ src/stores/useAuthStore.js
@@ -7,26 +7,37 @@ import { BASE_URL } from '../constants/constant.js';
 export const useAuthStore = defineStore('auth', () => {
   const router = useRouter();
   const user = ref(JSON.parse(localStorage.getItem('user')) || null);
-  const token = ref(localStorage.getItem('token') || null); 
+  const usertoken = ref(JSON.parse(localStorage.getItem('usertoken')) || null);
 
   const handleLogin = async (payload) => {
     try {
       const response = await axios.post(`${BASE_URL}/user/login`, payload);
-  
-      const successResponse = { success: true, message: response.data.message || 'Login successful!' };
-  
+
+      const successResponse = {
+        success: true,
+        message: response.data.message || 'Login successful!'
+      };
+
       user.value = response.data.user;
-      token.value = response.data.token;
-  
+      usertoken.value = response.data.usertoken;
+
       localStorage.setItem('user', JSON.stringify(response.data.user));
-      localStorage.setItem('token', response.data.token); 
-      router.push('/polls'); 
-  
+      localStorage.setItem(
+        'usertoken',
+        JSON.stringify(response.data.usertoken)
+      );
+      router.push('/polls');
+
       return successResponse;
     } catch (error) {
-      return { success: false, message: error.response?.data?.message || 'An error occurred. Please try again.' };
+      return {
+        success: false,
+        message:
+          error.response?.data?.message ||
+          'An error occurred. Please try again.'
+      };
     }
   };
 
-  return { user, token, handleLogin };
+  return { user, usertoken, handleLogin };
 });
